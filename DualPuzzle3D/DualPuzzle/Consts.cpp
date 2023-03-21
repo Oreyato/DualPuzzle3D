d01db87f023ea23fd377adadbddca7185bbd6d83
oreyato <oreyato.pro@gmail.com>
2023-03-21 17:02:21 +0100

    feat: consts

---
+++ DualPuzzle3D/DualPuzzle/Consts.cpp
@@ -0,0 +1,4 @@
+#include "Consts.h"
+
+const float Consts::TILE_LENGTH{ 100.0f };
+const float Consts::TILE_WIDTH{ 100.0f };
---
+++ DualPuzzle3D/DualPuzzle/Consts.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Consts
+{
+public:
+	// -- Tile --
+	static const float TILE_LENGTH;
+	static const float TILE_WIDTH;
+
+};
+
